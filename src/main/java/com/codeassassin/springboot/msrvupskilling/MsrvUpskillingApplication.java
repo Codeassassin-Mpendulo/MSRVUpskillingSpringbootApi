9fecb0b7f8d122f1f588e9e176c9e734742fbabd
MpenduloNxumalo <MpenduloN@discovery.co.za>
2023-09-11 11:44:54 +0200

    initial commit

---
+++ src/main/java/com/codeassassin/springboot/msrvupskilling/MsrvUpskillingApplication.java
@@ -0,0 +1,13 @@
+package com.codeassassin.springboot.msrvupskilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsrvUpskillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsrvUpskillingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeassassin/springboot/msrvupskilling/MsrvUpskillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeassassin.springboot.msrvupskilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsrvUpskillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
